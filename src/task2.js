2c49771c85daba6a625029bdaacc273e46a78c56
Vijay Bammidi <vijay.bammidi@thomsonreuters.com>
2019-11-26 15:16:03 +0530

    CSV to text file

--- src/task2.js
+++ src/task2.js
@@ -1,12 +1,8 @@
+const fs = require('fs');
+const csvFilePath='node_mentoring_t1_2_input_example.csv';
+const csv = require('csvtojson');
 
-(() => {
-const csvFilePath='node_mentoring_t1_2_input_example.csv'
-const csv = require('csvtojson')
- csv()
-.fromFile(csvFilePath)
-.then((jsonObj)=>{
-	console.log(jsonObj);
-})
-
-const jsonArray=  csv().fromFile(csvFilePath);
-})();
+ csv().fromFile(csvFilePath)
+      .then((jsonObj)=>{
+        fs.writeFile('./task2.txt', JSON.stringify(jsonObj), () => {});
+      });
