1135451623bd78319d2269ea4851ffc78a2627b6
Vijay Bammidi <vijay.bammidi@thomsonreuters.com>
2019-12-05 17:10:55 +0530

    Removed unnecessary commented code

--- src/task2.js
+++ src/task2.js
@@ -8,33 +8,3 @@ fs.createReadStream(paths.BASE_CSV_PATH)
         .pipe(csv())
         .on('data', (data) =>  writeText.write(data))
         .on('error', util);
-
-
-
-
-
-
-
-
-
-    /*
-====================================================== Practice ======================================    
-    const fs = require('fs');
-const es = require('event-stream');
-const csvFilePath='node_mentoring_t1_2_input_example.csv';
-const readCSv = fs.createReadStream(csvFilePath);
-const writeText = fs.createWriteStream('node_mentoring_t1_2_input_example.txt');
-
-
- const csv = require('csvtojson')
- csv().fromFile(csvFilePath).then((jsonObj) => {
-    writeText.write(JSON.stringify(jsonObj, null, 2));
-}); */
-
-/* const csv = require('csv-parser');
-const results = [];
-fs.createReadStream(csvFilePath).pipe(csv())
-  .on('data', (data) => results.push(data))
-  .on('end', () => {
-    writeText.write(JSON.stringify(results, null, 2));
-}); */  
