6395bd056dfe381f954586dc40ceec4cf2d43be1
Vijay Bammidi <vijay.bammidi@thomsonreuters.com>
2020-04-13 22:31:59 +0530

    Code Review comments

--- src/services/userService.spec.js
+++ src/services/userService.spec.js
@@ -1,8 +1,6 @@
-//let userModel = require("../database/models").Users;
-let UserService = require("../services/userService");
-// let expect = require('jest')
-let data = { login: "shiva", password: "rk@57", id: "idgyk" };
-let mockModel = {
+const UserService = require("../services/userService");
+const data = { login: "test", password: "rk@57", id: "idgyk" };
+const mockModel = {
   create: () => {
     return { dataValues: [data] };
   },
@@ -16,23 +14,23 @@ let mockModel = {
     return [null, { ...data, isdeleted: true }];
   }
 };
-let userService = new UserService(mockModel);
+const userService = new UserService(mockModel);
 
 describe("User Service", () => {
   test("it should create a user", async () => {
-    let result = await userService.createUser(data);
+    const result = await userService.createUser(data);
     expect(result).toEqual([data]);
   });
   test("it should find a user by id", async () => {
-    let result = await userService.getUser(data.id);
+    const result = await userService.getUser(data.id);
     expect(result).toEqual(data);
   });
   test("it should mark as deleted by id", async () => {
-    let result = await userService.deleteUser(data.id);
+    const result = await userService.deleteUser(data.id);
     expect(result.isdeleted).toBeTruthy();
   });
   test("it should find all the matched terms", async () => {
-    let result = await userService.searchUser(5,'shiva');
+    const result = await userService.searchUser(5,'test');
     expect(result).toEqual([data]);
   });
 });
